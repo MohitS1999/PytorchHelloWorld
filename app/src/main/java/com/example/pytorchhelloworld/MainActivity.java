1af721283a4782ad43e8ab3846014320ee5e969d
Mohit Singh <mohitsingh@Mohits-MacBook-Air.local>
2023-12-03 16:09:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pytorchhelloworld/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.pytorchhelloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.pytorch.IValue;
+import org.pytorch.LiteModuleLoader;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.name);
+        Bitmap bitmap = null;
+        Module module = null;
+        try {
+            // creating bitmap from packaged into app android asset 'image.jpg',
+            // app/src/main/assets/image.jpg
+            bitmap = BitmapFactory.decodeStream(getAssets().open("lion.jpg"));
+            // loading serialized torchscript module from packaged into app android asset model.pt,
+            // app/src/model/assets/model.pt
+            module = LiteModuleLoader.load(assetFilePath(this, "model.pt"));
+        } catch (IOException e) {
+            Log.w("PytorchHelloWorld", "Error reading assets", e);
+            finish();
+        }
+        imageView.setImageBitmap(bitmap);
+        Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
+                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+        Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+        float[] scores = outputTensor.getDataAsFloatArray();
+        float maxScore = -Float.MAX_VALUE;
+        int maxScoreIdx = -1;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > maxScore) {
+                maxScore = scores[i];
+                maxScoreIdx = i;
+            }
+        }
+        String className = ImageNetClasses.IMAGENET_CLASSES[maxScoreIdx];
+        textView.setText(className);
+        Log.d(TAG, "onCreate: "+className);
+
+    }
+
+    public static String assetFilePath(Context context, String assetName) throws IOException {
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
+}
